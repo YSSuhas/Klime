25b0a4443d809375031dc3e2e79948eb44d4027a
YSSuhas <yssuhas@gmail.com>
2021-08-01 20:20:04 +0530

    Added footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Footer.css';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 function Footer() {
     return (
-        <div className="Footer">
-            By YSS
+        <div className="footer">
+            <a href="https://www.linkedin.com/in/yssuhas/">
+                <LinkedInIcon fontSize="small" />
+            </a>
+            <p>By YSS</p>
+            <a href="https://github.com/YSSuhas">
+                <GitHubIcon fontSize="small" />
+            </a>
         </div>
     )
 }
